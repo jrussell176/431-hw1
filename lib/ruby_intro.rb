42809607a99c52cf530974a4a57c38c2e44d5da7
Jared Russell <jaredrussell176@gmail.com>
2017-02-04 00:15:38 +0000

    COmmitting homework

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,81 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0){|s,x| s + x }
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.size == 0
+    return 0
+  elsif arr.size == 1
+    return arr[0]
+  else
+    sorted_arr = arr.sort
+    top_two_values = sorted_arr[-2..sorted_arr.size]
+    return sum(top_two_values)
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  while arr.size > 1
+    num = arr.pop
+    for i in arr
+      if i + num == n
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  consonants = ['a','e','i','o','u']
+  if s[0] =~ /[A-Za-z]/
+    if s.size == 0
+      return false
+    else
+      return !consonants.include?(s[0].downcase)
+    end
+  end
+  return false
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  i = s.to_i(2)
+  if s == '0'
+    return true
+  elsif i == 0
+    return false
+  elsif i % 4 == 0
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price 
+  
+  def initialize(isbn, price)
+    unless isbn.size > 0
+      raise ArgumentError.new("No empty strings allowed")
+    end
+    unless price > 0
+      raise ArgumentError.new("Price must be greater than 0")
+    end
+    @isbn = isbn
+    @price = price
+  end
+  
+  def price_as_string
+    return '$%.2f' % @price
+  end
 end
